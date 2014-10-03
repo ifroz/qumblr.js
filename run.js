8bc38c783e3fb92d1969ef87f0c7a63fe8092d60
Laci Szűcs <ifrozen@gmail.com>
2014-10-03 14:17:11 +0200

    node-log-demultiplexer now asks for config first

--- run.js
+++ run.js
@@ -1,9 +1,10 @@
 'use strict';
 var qumblr = require('./src/index.js');
-
-var logger = require('node-log-demultiplexer').createTracker('tumblrQueue');
-
 var config = require('./src/config');
+var logger = require('node-log-demultiplexer')(
+      config.get('logger')).
+    createTracker('tumblrQueue');
+
 var tumblrCredentials = {
   consumer_key: config.get('private:TUMBLR_APP_KEY'),
   consumer_secret: config.get('private:TUMBLR_APP_SECRET'),
